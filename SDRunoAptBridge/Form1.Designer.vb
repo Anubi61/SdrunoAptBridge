1fbb82c901d82023193096ab112eb3794b3564b9
Anubi61 <110119241+Anubi61@users.noreply.github.com>
2022-07-27 21:36:19 +0200

    Add files via upload

---
+++ SDRunoAptBridge/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "APT Comm Port"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(120, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(67, 23)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(350, 14)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(67, 23)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "SDRUno Comm Port"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(460, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 15)
+        Me.Label3.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(603, 97)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Main"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ SDRunoAptBridge/Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports System
+Imports System.IO.Ports
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        GetSerialPortNames()
+
+
+    End Sub
+
+    Sub GetSerialPortNames()
+        ' Show all available COM ports.
+
+        Dim AvailablePorts() As String = SerialPort.GetPortNames()
+        Dim port As String
+        For Each port In AvailablePorts
+
+            ComboBox1.Items.Add(port)
+            ComboBox2.Items.Add(port)
+        Next port
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyCOMPortin As SerialPort
+
+
+        Dim comin = ComboBox1.SelectedItem
+
+        If ComboBox1.SelectedItem = "" Then
+            comin = ComboBox1.SelectedItem(1)
+        End If
+
+        MyCOMPortin = New SerialPort()
+        MyCOMPortin.PortName = comin   'Assign the port name to the MyCOMPort object
+        MyCOMPortin.BaudRate = 9600     'Assign the Baudrate to the MyCOMPort object
+        MyCOMPortin.Parity = Parity.None   'Parity bits = none  
+        MyCOMPortin.DataBits = 8             'No of Data bits = 8
+        MyCOMPortin.StopBits = StopBits.One  'No of Stop bits = 1
+
+
+
+        AddHandler MyCOMPortin.DataReceived, AddressOf DataReceivedHandler
+
+        MyCOMPortin.Open()
+
+    End Sub
+
+
+
+    Private Sub DataReceivedHandler(
+                        sender As Object,
+                        e As SerialDataReceivedEventArgs)
+        Dim sp As SerialPort = CType(sender, SerialPort)
+        Dim indata As String = sp.ReadExisting()
+        If indata.Length < 2 Then
+            Exit Sub
+
+        End If
+        If indata.Length > 2 Then
+            indata = Strings.Right(indata, 2)
+
+        End If
+        SendFreq(indata)
+
+    End Sub
+
+    Sub SendFreq(freq As String)
+        Dim MyCOMPortout As SerialPort
+        MyCOMPortout = New SerialPort()
+        ComboBox2.Invoke(Sub()
+                             Dim comout = ComboBox2.SelectedItem
+
+                             If ComboBox2.SelectedItem = "" Then
+                                 comout = ComboBox2.SelectedItem(2)
+                             End If
+                             MyCOMPortout.PortName = comout
+                             MyCOMPortout.BaudRate = 9600    'Assign the Baudrate to the MyCOMPort object
+                             MyCOMPortout.Parity = Parity.None   'Parity bits = none  
+                             MyCOMPortout.DataBits = 8             'No of Data bits = 8
+                             MyCOMPortout.StopBits = StopBits.One  'No of Stop bits = 1
+                         End Sub)
+
+        MyCOMPortout.Open()
+
+
+                             Select Case freq
+                                 Case "F1"
+                MyCOMPortout.Write("FA00137100000;")
+                Label3.Invoke(Sub()
+                                                       Label3.Text = "137.100 Mhz"
+                                                   End Sub)
+                                 Case "F3"
+                MyCOMPortout.Write("FA00137500000;")
+                Label3.Invoke(Sub()
+
+                                                       Label3.Text = "137.500 Mhz"
+                                                   End Sub)
+                                 Case "F4"
+                MyCOMPortout.Write("FA00137620000;")
+                Label3.Invoke(Sub()
+                                                       Label3.Text = "137.620 Mhz"
+                                                   End Sub)
+                                 Case "F5"
+                MyCOMPortout.Write("FA00137912500;")
+                Label3.Invoke(Sub()
+                                                       Label3.Text = "137.9125 Mhz"
+                                                   End Sub)
+
+                             End Select
+                             MyCOMPortout.Close()
+                         End Sub
+
+End Class
